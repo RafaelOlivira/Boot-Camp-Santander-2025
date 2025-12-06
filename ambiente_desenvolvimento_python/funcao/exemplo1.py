822f8d91854eef8155162ec7b7f56fa43e5f64b5
Rafael <rafaeloliveirasouzasil@gmail.com>
2025-12-06 18:36:56 -0300

    Função

---
+++ ambiente_desenvolvimento_python/funcao/exemplo1.py
@@ -0,0 +1,12 @@
+def exibir_mensagem():
+    print("Olá mundo!")
+    
+def exibir_mensagem2(nome):
+    print(f"Seja bem vindo {nome}!")
+
+def exibir_mensagem3(nome="Anônimo"):
+    print(f"Seja bem vindo {nome}!")
+
+exibir_mensagem()
+exibir_mensagem2("Rafael")
+exibir_mensagem3()
---
+++ ambiente_desenvolvimento_python/funcao/exemplo2.py
@@ -0,0 +1,5 @@
+def media(a,b):
+    result = (a + b) / 2
+    return result
+
+print(media(12,10))
